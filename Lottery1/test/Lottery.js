50b4230aa19ed96ea6d9e126a275842e19b83a72
something <nnebp@users.noreply.github.com>
2018-06-16 11:44:14 -0400

    async await format for tests

--- Lottery1/test/Lottery.js
+++ Lottery1/test/Lottery.js
@@ -1,6 +1,7 @@
-var Lottery = artifacts.require("./Lottery.sol");
-
+const Lottery = artifacts.require("./Lottery.sol");
+/*
 contract('Lottery', function(accounts) {
+
   it("should assert true", function() {
     var hiAngely;
     return Lottery.deployed().then(function(instance){
@@ -10,5 +11,28 @@ contract('Lottery', function(accounts) {
     }).then(function(result){
       console.log("Result string = ",result);
     });
+  });
+
+});
+*/
+
+contract ('Lottery', (accounts) => {
+
+  let lottery;
+
+  const owner = accounts[0];
+  beforeEach(async() => {
+    lottery = await Lottery.new();
+  });
+
+  it("should assert true", async function() {
+
+    let hi  = await lottery.hi( );
+
+    await lottery.enter(5, {from: accounts[1]});
+    let guessed = await lottery.getGuess( accounts[1] );
+
+    console.log("Result string = ", hi);
+    console.log("guess is = ", guessed.toNumber());
   });
 });

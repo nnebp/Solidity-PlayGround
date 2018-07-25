9e49272f82b7afc55001a6b244e4b5736aa5df5a
something <nnebp@users.noreply.github.com>
2018-07-25 19:55:38 -0400

    size not magic number

--- Lottery1/contracts/Lottery.sol
+++ Lottery1/contracts/Lottery.sol
@@ -2,10 +2,12 @@ pragma solidity ^0.4.17;
 
 contract Lottery {
 
+    uint8 constant size = 100;
     address public owner;
+
     //TODO size magic number
     //would this be more efficicent as a stack?
-    address[100] public participants;
+    address[size] public participants;
 
     uint8 maxParticipants;
     uint8 minParticipants;
@@ -27,12 +29,12 @@ contract Lottery {
     // Constructor
     function Lottery() public {
         owner = msg.sender;
-        maxParticipants = 100;
+        maxParticipants = size;
     }
 
     function join(uint8 guess, uint256 amount) public payable{
         require(msg.value == amount);
-        require (guess <= 100 && guess >= 1);
+        require (guess <= size && guess >= 1);
         require(joinedParticipants + 1 < maxParticipants);
         //cant buy the same number twice
         //require(entries[guess] != msg.sender);
@@ -42,14 +44,13 @@ contract Lottery {
         entries[guess] = msg.sender;
     }
 
-    //TODO make this owner only then switch to oracalize
     function chooseWinner(uint8 winningNum) public OwnerOnly{
         address winner = entries[winningNum];
         //TODO this is deprecated i guess?
         winner.transfer(this.balance);
 
         //delete everything in the array/mapping
-        for (uint8 i = 0; i < 100; i++) {
+        for (uint8 i = 0; i < size; i++) {
             delete participants[i];
             delete entries[i];
         }
